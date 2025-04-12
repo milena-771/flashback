b93d60fe55554faa52b1b4006e1d3cd85cc2c9ba
milena <milena771@hotmail.fr>
2025-04-12 14:18:19 +0200

    rm test

--- flashback-ui/src/components/beforeAuthentication/SignIn.vue
+++ flashback-ui/src/components/beforeAuthentication/SignIn.vue
@@ -18,7 +18,6 @@ export default {
     methods:{
         async submit(){ 
             try{
-                console.log("test");
                 localStorage.clear();
                 const response = await this.$axios.post('/sign-in', this.inputs);       
                 if(response.data.role === "ADMIN"){
