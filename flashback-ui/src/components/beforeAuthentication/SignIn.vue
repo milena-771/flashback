bd2eeecaf89150e382364026be3b451ba1bcc2a7
milena <milena771@hotmail.fr>
2025-04-13 11:47:51 +0200

    test 1

--- flashback-ui/src/components/beforeAuthentication/SignIn.vue
+++ flashback-ui/src/components/beforeAuthentication/SignIn.vue
@@ -18,7 +18,7 @@ export default {
     methods:{
         async submit(){ 
             try{
-                console.log("success")
+                console.log("success1")
                 console.log("github")
                 localStorage.clear();
                 const response = await this.$axios.post('/sign-in', this.inputs);       
