110d9fd44e367e81c342abd9d7f041f29c532695
milena <milena771@hotmail.fr>
2025-05-18 12:20:43 +0200

    test build, run api and ui  #3

--- flashback-ui/src/components/beforeAuthentication/SignIn.vue
+++ flashback-ui/src/components/beforeAuthentication/SignIn.vue
@@ -18,7 +18,6 @@ export default {
     methods:{
         async submit(){ 
             try{
-                console.log("test ui build success");
                 localStorage.clear();
                 const response = await this.$axios.post('/sign-in', this.inputs);       
                 if(response.data.role === "ADMIN"){
