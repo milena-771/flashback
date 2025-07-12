2571a4418c52467abc401c3a17472cf1da4343ab
milena <milena771@hotmail.fr>
2025-07-12 11:59:14 +0200

    test log

--- flashback-api/src/main/java/co/simplon/flashback/FlashbackApiApplication.java
+++ flashback-api/src/main/java/co/simplon/flashback/FlashbackApiApplication.java
@@ -1,14 +1,17 @@
 package co.simplon.flashback;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlashbackApiApplication {
 
-    public static void main(String[] args) {
-	SpringApplication.run(FlashbackApiApplication.class,
-		args);
-    }
+	public static void main(String[] args) {
+		Logger LOG = LogManager.getLogger(FlashbackApiApplication.class);
+		LOG.info("Démarrage de l'application FlashbackApiApplication");
+		SpringApplication.run(FlashbackApiApplication.class, args);
+	}
 
 }
